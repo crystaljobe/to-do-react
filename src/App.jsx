f70a8e083008e03d8b87aed105e95c56a6f0d3c9
crystaljobe <crystaljobe29@gmail.com>
2024-02-29 19:24:22 -0700

    commit to save changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,29 @@ import tasksData from "./assets/tasks.json"
 
 function App() {
   const [tasks, setTasks] = useState(tasksData);
+  const [newTaskInput, setNewTaskInput] = useState('')
 
-  const renderTask = (task) => {
-    console.log(task)
-    return`ID: ${task.id}, ${task.description}, COMPLETED: ${task.completed ? "Yes" : "No"}`
-  }
 
-  const addNewTask = (e) => {
-    e.preventDefault();
-    newTask = {id: tasks.length + 1}
-    setTasks(tasks => [...tasks, newTask]);
+   const addNewTask = (e) => {
+     const newTask = {id: tasks.length + 1, description: newTaskInput, completed: false};
+     setTasks([...tasks, newTask]);
+   };
+
+  const handleNewTaskInput = (event) => {
+    setNewTaskInput(event.target.value);
   };
-  // const newTask = { id: 16, task: "Buy groceries", completed: false };
 
   return (
     <>
       <h1>Attempting to Conquer the Chaos</h1>
       <h2>Current Procrastination List:</h2>
       <div className="card">
-        <form onSubmit={(e) => addNewTask(e)}>
-        <input id="taskInput" type="text" placeholder='New Task'></input>
-        <button id="sumbitButton">Add New Task</button>
-        </form>
+        <input id="taskInput" type="text" placeholder={'New Task'} onChange={handleNewTaskInput} value={newTaskInput}></input>
+        <button onClick={(e) => addNewTask(e)}>Add New Task</button>
       </div>
       <div className="tasksContainer">
         <ul id="tasks">
-          {tasks.map((task, i) => <li key={i}>{renderTask(task)}</li>)}
+          {tasks.map((task, i) => !task.completed && <li key={i}>{task.description}</li>)}
         </ul>
       </div>
     </>
